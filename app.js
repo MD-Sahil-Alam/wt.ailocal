1686e67293b23c9674ba6a827598fa6be381d081
MD-Sahil-Alam <sahilalam.786.18@gmail.com>
2024-02-08 20:33:38 +0530

    new auth

--- app.js
+++ app.js
@@ -1,75 +1,4 @@
 // const qrcode = require('qrcode-terminal');
-// const { Client, LocalAuth } = require('whatsapp-web.js');
-// const OpenAI = require('openai');
-// require('dotenv').config();
-// const http = require('http');
-
-// const server = http.createServer((req, res) => {
-//   res.end('Hello, World!');
-// });
-
-// const PORT = 64840;
-
-// const whatsapp = new Client({
-//     authStrategy: new LocalAuth(),
-// });
-
-// // whatsapp.on('qr', (qr) => {
-// //         qrcode.generate(qr, { small: true });
-// //     });
-
-// const openaiApiKey = process.env.OPENAI_KEY;
-// const openai = new OpenAI({ apiKey: openaiApiKey });
-
-
-// whatsapp.on('message', async (msg) => {
-//     if (msg.body.charAt(0) === '/') {
-//         msg.reply("Please wait for few seconds...");
-//         try {
-//             const query = msg.body.substring(1);
-//             const completion = await openai.chat.completions.create({
-//                 messages: [
-//                     { role: "system", content: "You are a teaching assistant (build/made by Md.Sahil Alam.) you can answer questions of all the subject present in both stream commerce as well as in science of ISC class 11th . You have access to a retrieval tool that lets you search for relevant information from a database of textbooks and articles. For each question, you should provide a short and concise answer in simple language, using bullet points, tables, graphs, or code snippets as needed. You should also cite the source of your information using the retrieval tool." },
-//                     { role: "user", content: `${query}` },
-//                 ],
-//                 model: "gpt-3.5-turbo",
-//             });
-
-//             const chatId = msg.from;
-//             await whatsapp.sendMessage(chatId, completion.choices[0].message.content);
-//             console.log(`Message sent successfully! to ${chatId}`);
-//         } catch (error) {
-//             console.error('An error occurred:', error);
-//             msg.reply("Sorry, something went wrong 🫤. Please try again later");
-//         }
-//     }
-// });
-
-// whatsapp.on('ready', async () => {
-//     console.log('WhatsApp client is ready!');
-//      // Send keep alive message to the group periodically
-//      setInterval(async () => {
-//         try {
-//             await whatsapp.sendMessage('916201818940@c.us', 'we are working');
-//             console.log('Keep alive message sent to group');
-//         } catch (error) {
-//             console.error('Error sending keep alive message:', error);
-//         }
-//     }, 3 * 60 * 1000); // Send a message every 10 minutes
-// });
-
-// server.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-//   });
-
-// whatsapp.initialize();
-
-
-
-
-
-
-
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const OpenAI = require('openai');
 require('dotenv').config();
@@ -87,6 +16,8 @@ server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
+
+
 // Function to initialize WhatsApp client
 const initializeWhatsApp = async () => {
     try {
@@ -95,16 +26,16 @@ const initializeWhatsApp = async () => {
             }),
         });
 
+        // whatsapp.on('qr', (qr) => {
+        //     qrcode.generate(qr, { small: true });
+        // });
+
         const openaiApiKey = process.env.OPENAI_KEY;
         const openai = new OpenAI({ apiKey: openaiApiKey });
 
         // Handle incoming messages
         whatsapp.on('message', async (msg) => {
-            if (msg.body === 'Are u working') {
-                await whatsapp.sendMessage('916201818940@c.us', 'yes');
-                console.log('checking ok');
-            }
-            if (msg.body.charAt(0) === '/') {
+            if (msg.body.charAt(0) === '.') {
                 msg.reply("Please wait for few seconds...");
                 try {
                     const query = msg.body.substring(1);
@@ -150,6 +81,7 @@ const initializeWhatsApp = async () => {
         const restartWhatsApp = async () => {
             console.log('Restarting WhatsApp client...');
             await initializeWhatsApp(); // Reinitialize WhatsApp client
+            console.log('WhatsApp client is ready!');
         };
 
             // Restart WhatsApp client every 7 minutes
